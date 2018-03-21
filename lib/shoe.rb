df7aadfdf1208655a0be93ec7dad9540df60b4b3
Joseph Vint <josephvint@me.com>
2018-03-21 21:51:30 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,7 +7,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand if BRANDS[0] == nil || BRANDS.detect{|shoe| shoe != brand}
+    BRANDS << brand unless BRANDS.include?(brand)
   end
 
   def cobble
